8e1aea70c859bc2ea59d47f9d512640b61c0a368
Qhallim <qayyimhallim@gmail.com>
2026-06-28 17:21:31 -0400

    added componets directory

---
+++ client/src/components/TestHeader.jsx
@@ -0,0 +1,9 @@
+function TestHeader() {
+  return (
+    <h2 style={{ color: "red" }}>
+      Hello World! THis is a test component.
+    </h2>
+  );
+}
+
+export default TestHeader;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import TestHeader from "../components/TestHeader";
+
 export default function Home() {
-  return (
-    <div className="container heading text-black">
-      <h1>Search Party test text!!</h1>
-    </div>
-  );
+    return (
+        <>
+
+            <TestHeader />
+
+
+            <div className="container heading text-black">
+                <h1>Search Party test text!!</h1>
+            </div>
+
+
+
+        </>
+    );
 }
