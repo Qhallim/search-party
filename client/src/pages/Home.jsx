aa4a7e9700d5f279fd3e37586348a7a194f22f6a
Qayyim Hallim <qayyim.hallim80@myhunter.cuny.edu>
2026-07-02 14:17:09 -0400

    Implemented basic Itemcard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
+
 import TestHeader from "../components/TestHeader";
+import ItemCard from "../components/ItemCard";
 
 export default function Home() {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/items")
+            .then((res) => res.json())
+            .then((data) => setItems(data));
+    }, []);
+
+
     return (
         <>
 
-            <TestHeader />
+            <div style={{ backgroundColor: "green", minHeight: "100vh" }}>
 
+                <TestHeader />
 
-            <div className="container heading text-black">
-                <h1>Search Party test text!!</h1>
+                <div className="container heading text-black">
+                    <h1>Search Party test text!!</h1>
+                </div>
+
+                <div className="container mt-4">
+                    {items.map((item) => (
+                        <ItemCard key={item._id} item={item} />
+                    ))}
+                </div>
+
             </div>
-
-
 
         </>
     );
